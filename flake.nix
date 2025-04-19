a7583e0f7151d7e5d3ff7ec327997af3959f13a4
Sean Outram <outramsean0@gmail.com>
2025-04-19 19:19:37 +0000

    ISO!

--- flake.nix
+++ flake.nix
@@ -27,5 +27,19 @@
           }
         ];
       };
+      ISO = nixpkgs.lib.nixosSystem {
+        system = "x86_64-linux";
+        specialArgs = attrs;
+        modules = [
+          home-manager.nixosModules.home-manager {
+            home-manager.useGlobalPkgs = true;
+            home-manager.useUserPackages = true;
+            home-manager.users.sean = import ./users/sean.nix;
+      #	    home-manager.users.root = import ./users/root.nix;
+          }
+	        "${nixpkgs}/nixos/modules/installer/cd-dvd/installation-cd-base.nix"
+	        ./hardware/iso.nix
+	      ];	
+      };
     };
 }
---
+++ hardware/iso.nix
@@ -0,0 +1,11 @@
+{ config, pkgs, lib, ... }:
+{
+  imports = [
+    ../shared/core.nix
+    ../shared/desktop.nix
+  ];
+  environment.etc."setup.sh".source = ../setup.sh;
+  environment.etc."setup.sh".mode = "0755";
+  services.getty.autologinUser = lib.mkForce "root";  
+  networking.networkmanager.enable = lib.mkForce false;
+}
