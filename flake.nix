838f7a35337f9f917e12253fcc14f3a17c552c9f
Sean Outram <me@sean.cyou>
2025-04-24 23:50:10 +0100

    Update flake.nix

--- flake.nix
+++ flake.nix
@@ -13,33 +13,34 @@
     {
       formatter.x86_64-linux = nixpkgs.legacyPackages.x86_64-linux.nixfmt-rfc-style;
       # replace 'joes-desktop' with your hostname here.
-      nixosConfigurations.gaius = nixpkgs.lib.nixosSystem {
-        system = "x86_64-linux";
-        specialArgs = attrs;
-        modules = [
-          ./hardware/thinkpad-l480.nix
-          ./machines/gaius.nix
-          home-manager.nixosModules.home-manager
-          {
-            home-manager.useGlobalPkgs = true;
-            home-manager.useUserPackages = true;
-            home-manager.users.sean = import ./users/sean.nix;
-          }
-        ];
-      };
-      ISO = nixpkgs.lib.nixosSystem {
-        system = "x86_64-linux";
-        specialArgs = attrs;
-        modules = [
-          home-manager.nixosModules.home-manager {
-            home-manager.useGlobalPkgs = true;
-            home-manager.useUserPackages = true;
-            home-manager.users.sean = import ./users/sean.nix;
-      #	    home-manager.users.root = import ./users/root.nix;
-          }
-	        "${nixpkgs}/nixos/modules/installer/cd-dvd/installation-cd-base.nix"
-	        ./hardware/iso.nix
-	      ];	
-      };
+      nixosConfigurations = { 
+	gaius = nixpkgs.lib.nixosSystem {
+	        system = "x86_64-linux";
+	        specialArgs = attrs;
+	        modules = [
+	          ./hardware/thinkpad-l480.nix
+	          ./machines/gaius.nix
+	          home-manager.nixosModules.home-manager
+	          {
+	            home-manager.useGlobalPkgs = true;
+	            home-manager.useUserPackages = true;
+	            home-manager.users.sean = import ./users/sean.nix;
+	          }
+	        ];
+	};
+	ISO = nixpkgs.lib.nixosSystem {
+	        system = "x86_64-linux";
+	        specialArgs = attrs;
+	        modules = [
+	          home-manager.nixosModules.home-manager {
+	            home-manager.useGlobalPkgs = true;
+	            home-manager.useUserPackages = true;
+	            home-manager.users.sean = import ./users/sean.nix;
+	      #	    home-manager.users.root = import ./users/root.nix;
+	          }
+		        "${nixpkgs}/nixos/modules/installer/cd-dvd/installation-cd-base.nix"
+		        ./hardware/iso.nix
+		      ];	
+	};
     };
 }
